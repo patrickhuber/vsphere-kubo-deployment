19339f2a6889fcde3e30cb7e5e403d77ef365f9a
Patrick <182398+patrickhuber@users.noreply.github.com>
2018-01-29 11:51:56 -0500

    removes multiple masters

--- deploy-kubo.sh
+++ deploy-kubo.sh
@@ -3,7 +3,6 @@ bosh deploy -d kubo ~/github.com/cloudfoundry-incubator/kubo-deployment/manifest
   -o ~/github.com/cloudfoundry-incubator/kubo-deployment/manifests/ops-files/iaas/vsphere/cloud-provider.yml \
   -o ops-files/one-az.yml \
   -o ops-files/deployment-name.yml \
-  -o ops-files/three-masters.yml \
   -v master_vm_type=default \
   -v worker_vm_type=default \
   -v deployment_name=kubo
