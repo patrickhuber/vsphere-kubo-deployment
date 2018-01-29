aa9a7bf5b257375f0307a9182c333d09e2c796ee
Patrick <182398+patrickhuber@users.noreply.github.com>
2018-01-29 12:05:02 -0500

    fixes bad path

--- deploy-kubo.sh
+++ deploy-kubo.sh
@@ -1,6 +1,6 @@
 bosh deploy -d kubo submodules/github.com/cloudfoundry-incubator/kubo-deployment/manifests/cfcr.yml \
   -o submodules/github.com/cloudfoundry-incubator/kubo-deployment/manifests/ops-files/vm-types.yml \
-  -o submodules/cloudfoundry-incubator/kubo-deployment/manifests/ops-files/iaas/vsphere/cloud-provider.yml \
+  -o submodules/github.com/cloudfoundry-incubator/kubo-deployment/manifests/ops-files/iaas/vsphere/cloud-provider.yml \
   -o ops-files/one-az.yml \
   -o ops-files/deployment-name.yml \
   -v master_vm_type=default \
--- int-kubo.sh
+++ int-kubo.sh
@@ -3,7 +3,6 @@ bosh int -d kubo submodules/github.com/cloudfoundry-incubator/kubo-deployment/ma
   -o submodules/github.com/cloudfoundry-incubator/kubo-deployment/manifests/ops-files/iaas/vsphere/cloud-provider.yml \
   -o ops-files/one-az.yml \
   -o ops-files/deployment-name.yml \
-  -o ops-files/three-masters.yml \
   -v master_vm_type=default \
   -v worker_vm_type=default \
   -v deployment_name=kubo
